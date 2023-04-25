4bc6046890e69e3dc6ca66b709d5a48762ef7bac
Bimbolin <osholowubimbola@gmail.com>
2023-04-25 10:25:26 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,7 @@
 /**
  * print_sign - Print the sign of a number
  * @n: The number to be checked
- * Return: 1 for positive num, -1 for negative num or zero for
- anything else
+ * Return: 1 for positive num, -1 for negative num or zero
  */
 
 int print_sign(int n)
