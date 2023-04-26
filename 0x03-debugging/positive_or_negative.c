6f903ce602ae2fbfea1bc20ea4f8e8d6f12d1028
Bimbolin <osholowubimbola@gmail.com>
2023-04-26 12:36:44 -0700

    debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,15 +2,25 @@
 /**
  * positive_or_negative - checks for positive or negative numbers
  *
- * @i: the number to be checked
+ * @i: is the number to be checked
  *
  * Return: always 0
  */
 
 void positive_or_negative(int i)
 {
-	if (i < 0)
-		printf("%d is negative\n", i);
-	else
-		printf("%d is positive\n", i);
+if (i < 0)
+{
+printf("%d is %s\n", i, "negative");
+}
+else if (i > 0)
+{
+printf("%d is %s\n", i, "positive");
+}
+else
+{
+printf("%d is %s\n", i, "zero");
+}
+return;
+
 }
